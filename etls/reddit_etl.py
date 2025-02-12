a52e3fb56dd0d3afeb2efb3f85a0aba2f8c1f1b9
Mateus Nazário <127886025+MateusNaza@users.noreply.github.com>
2025-02-12 13:29:48 +0000

    fix: correção do tipo de dado e avanço para erro 401

--- etls/reddit_etl.py
+++ etls/reddit_etl.py
@@ -1,10 +1,11 @@
 import praw
-from praw import reddit
+from praw import Reddit
 import sys
+import pandas as pd
 
-def connect_reddit(client_id, client_secret, user_agent) -> reddit:
+def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
     try:
-        reddit = praw.reddit(client_id=client_id,
+        reddit = praw.Reddit(client_id=client_id,
                              client_secret=client_secret,
                              user_agent=user_agent)
         print("connected to reddit!")
@@ -13,10 +14,21 @@ def connect_reddit(client_id, client_secret, user_agent) -> reddit:
         print(e)
         sys.exit(1)
 
-def extract_posts(reddit_instance: reddit, subreddit: str, time_filter: str, limit=None):
+def extract_posts(reddit_instance: Reddit, subreddit: str, time_filter: str, limit=None):
     subreddit = reddit_instance.subreddit(subreddit)
     posts = subreddit.top(time_filter=time_filter, limit=limit)
 
     posts_list = []
+    for post in posts:
+        posts_list.append({
+            'title': post.title,
+            'score': post.score,
+            'id': post.id,
+            'url': post.url,
+            'comms_num': post.num_comments,
+            'created': post.created,
+            'body': post.selftext
+        })
 
-    print(posts)
+    return posts_list
+
